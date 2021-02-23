00a6b5eabdb77714fa5acdf1a5294b7bd182cf5f
Daniel Wernz <dwernz@att.net>
2021-02-23 02:14:22 -0500

    Update calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -17,3 +17,11 @@ function division(num1, num2) {
 function calculator(num1, num2, operator) {
     return operator(num1, num2);
 }
+
+function quadraticFormula(a, b, c) {
+
+    var add = (-b + Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
+    var subtract = (-b - Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
+    
+    return [add, subtract];
+}
