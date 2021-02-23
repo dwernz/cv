dd8b3fdb7f9a8b5f126be2dc62f7225b5790b316
Daniel Wernz <dwernz@att.net>
2021-02-23 00:25:04 -0500

    Update drum-kit.js

--- scripts/drum-kit.js
+++ scripts/drum-kit.js
@@ -1,10 +1,10 @@
-var audio1 = new Audio("../sounds/drum-kit/tom-1.mp3");
-var audio2 = new Audio("../sounds/drum-kit/tom-2.mp3");
-var audio3 = new Audio("../sounds/drum-kit/tom-3.mp3");
-var audio4 = new Audio("../sounds/drum-kit/tom-4.mp3");
-var audio5 = new Audio("../sounds/drum-kit/snare.mp3");
-var audio6 = new Audio("../sounds/drum-kit/crash.mp3");
-var audio7 = new Audio("../sounds/drum-kit/kick-bass.mp3");
+var tom1 = new Audio("../sounds/drum-kit/tom-1.mp3");
+var tom2 = new Audio("../sounds/drum-kit/tom-2.mp3");
+var tom3 = new Audio("../sounds/drum-kit/tom-3.mp3");
+var tom4 = new Audio("../sounds/drum-kit/tom-4.mp3");
+var snare = new Audio("../sounds/drum-kit/snare.mp3");
+var crash = new Audio("../sounds/drum-kit/crash.mp3");
+var kick = new Audio("../sounds/drum-kit/kick-bass.mp3");
 
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
@@ -12,26 +12,44 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 function handleClick() {
     
-    if(this.innerHTML == "w") {
-        audio1.play();
-    }
-    else if(this.innerHTML == "a") {
-        audio2.play();
-    }
-    else if(this.innerHTML == "s") {
-        audio3.play();
-    }
-    else if(this.innerHTML == "d") {
-        audio4.play();
-    }
-    else if(this.innerHTML == "j") {
-        audio5.play();
-    }
-    else if(this.innerHTML == "k") {
-        audio6.play();
-    }
-    else if(this.innerHTML == "l") {
-        audio7.play();
-    }
+    var buttonInnerHTML = this.innerHTML;
+    
+    makeSound(buttonInnerHTML);
     
 }
+
+function makeSound(key) {
+    
+    switch(key) {
+        case "w":
+            tom1.play();
+            break;
+        case "a":
+            tom2.play();
+            break;
+        case "s":
+            tom3.play();
+            break;
+        case "d":
+            tom4.play();
+            break;
+        case "j":
+            snare.play();
+            break;
+        case "k":
+            crash.play();
+            break;
+        case "l":
+            kick.play();
+            break;
+        default:
+            console.log(this.innerHTML);
+            break;
+    }
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keydown", function(event) {
+   makeSound(event.key);
+});
