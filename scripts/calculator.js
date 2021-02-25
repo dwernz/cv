ac44342235161133b6156b2708cd062cb0fc19d6
Daniel Wernz <dwernz@att.net>
2021-02-24 19:42:20 -0500

    Day 4 update

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,27 +1,83 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
+var display = "";
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function division(num1, num2) {
-    return num1 / num2;
-}
-
-function calculator(num1, num2, operator) {
-    return operator(num1, num2);
-}
-
-function quadraticFormula(a, b, c) {
-
-    var add = (-b + Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
-    var subtract = (-b - Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
+$("button").click(function() {
+    if($(this).hasClass("clear")) {
+        display = "0";
+        $("h4").text(display);
+        console.log(display);
+    }
+    if($(this).hasClass("parentheses")) {
+        alert("()");
+    }
+    if($(this).hasClass("percent")) {
+        alert("%");
+    }
+    if($(this).hasClass("divide")) {
+        alert("/");
+    }
+    if($(this).hasClass("multiply")) {
+        alert("*");
+    }
+    if($(this).hasClass("subtract")) {
+        alert("-");
+    }
+    if($(this).hasClass("add")) {
+        add(display);
+    }
+    if($(this).hasClass("decimal")) {
+        alert(".");
+    }
+    if($(this).hasClass("switch")) {
+        alert("+/-");
+    }
+    if($(this).hasClass("equals")) {
+        alert("=");
+    }
+    if(display.length < 15) {
+        if(display[0] == "0") {
+            display = "";
+        }
+        if($(this).hasClass("one")) {
+            display = display + "1";
+            $("h4").text(display);
+            console.log(display);
+        }
+        if($(this).hasClass("two")) {
+            display = display + "2";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("three")) {
+            display = display + "3";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("four")) {
+            display = display + "4";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("five")) {
+            display = display + "5";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("six")) {
+            display = display + "6";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("seven")) {
+            display = display + "7";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("eight")) {
+            display = display + "8";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("nine")) {
+            display = display + "9";
+            $("h4").text(display);
+        }
+        if($(this).hasClass("zero")) {
+            display = display + "0";
+            $("h4").text(display);
+        }
+    }
     
-    return [add, subtract];
-}
+});
